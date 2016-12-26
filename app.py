d90e70a068cc0154c2edd3b78292dfb1e12008a6
Otto Modinos <ottomodinos@gmail.com>
2016-12-26 21:59:24 +0200

    Remove a statement

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 import wx
 
 
-SELECT Drug.name,Sell.price,Drug.formula 
-FROM Sell JOIN Drug
-ON Sell.pharmacy_id=Drug.id;
+STATEMENT1 = """
+SELECT Drug.name,Sell.price,Drug.formula FROM
+  Sell
+  JOIN Drug ON Sell.pharmacy_id=Drug.id;"""
 
-SELECT Doctor.*,Patient.name
-FROM Doctor LEFT JOIN Patient
-ON Doctor.id=Patient.id;
-
-SELECT Doctor.name,max(exp)
-FROM Doctor
-WHERE specialty = 'surgeon'
-GROUP BY Doctor;
+STATEMENT2 = """
+SELECT Doctor.*, Patient.name FROM
+  Doctor
+  LEFT JOIN Patient ON Doctor.id=Patient.id;"""
