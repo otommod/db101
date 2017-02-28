979a8f7bb92dc77e4cbb1e010037555998abbe5e
Otto Modinos <ottomodinos@gmail.com>
2017-03-01 00:25:12 +0200

    stuffs again

--- app.py
+++ app.py
@@ -46,9 +46,15 @@ FROM Contract
 ORDER BY end_date;
 """
 
-conn = psycopg2.connect("dbname=db101 user=otto")
-cur = conn.cursor()
 
+class Databaser:
+    def __init__(self, user, dbname):
+        self.conn = psycopg2.connect(dbname=dbname, user=user)
+
+    def num_of_patients(self):
+        with self.conn.cursor as cur:
+            cur.execute(GROUP_BY)
+            return cur.fetchone()[0]
 
 
 # Window classes
@@ -56,39 +62,13 @@ class MainWindow(wx.Frame):
     TITLE = "The Databaser 3000"
     SIZE = (380, 380)
 
-    def __init__(self, parent, title):
+    def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent,
                                          title=MainWindow.TITLE,
                                          size=MainWindow.SIZE)
 
         panel = wx.Panel(self)
         vbox = wx.BoxSizer(wx.VERTICAL)
-
-        # Input
-        # input_vbox = wx.BoxSizer(wx.VERTICAL)
-        # input_hbox = wx.BoxSizer(wx.HORIZONTAL)
-
-        # input_header = wx.StaticText(panel, label='Insert number')
-        # input_vbox.Add(input_header,
-        #                flag=wx.ALIGN_CENTER | wx.ALL, border=20)
-
-        # self.input_box = FlashingTextCtrl(panel)
-        # input_hbox.Add(self.input_box,
-        #                flag=wx.RIGHT, border=10, proportion=1)
-
-        # self.from_base_button = BaseSelectButton(panel)
-        # input_hbox.Add(self.from_base_button)
-
-        # input_vbox.Add(input_hbox,
-        #                flag=wx.EXPAND | wx.LEFT | wx.RIGHT, border=80)
-        # vbox.Add(input_vbox,
-        #          flag=wx.EXPAND)
-
-
-        line = wx.StaticLine(panel)
-        vbox.Add(line,
-                 flag=wx.EXPAND | wx.ALL, border=50)
-
 
         # Output
         # output_vbox = wx.BoxSizer(wx.VERTICAL)
@@ -111,23 +91,20 @@ class MainWindow(wx.Frame):
         # vbox.Add(output_vbox,
         #          flag=wx.EXPAND)
 
+        line = wx.StaticLine(panel)
+        vbox.Add(line,
+                 flag=wx.EXPAND | wx.ALL, border=50)
 
         # Buttons
         buttons_vbox = wx.BoxSizer(wx.VERTICAL)
         buttons_hbox = wx.BoxSizer(wx.HORIZONTAL)
-
-        exit_button = wx.Button(panel, label='Exit')
-        exit_button.Bind(wx.EVT_BUTTON, self.exit)
-        buttons_hbox.Add(exit_button,
-                         flag=wx.ALIGN_LEFT)
-
-        buttons_hbox.AddStretchSpacer()
 
         # convert_button = wx.Button(panel, label='Convert')
         # convert_button.SetBackgroundColour('#4ebc06')
         # convert_button.Bind(wx.EVT_BUTTON, self.convert)
         # buttons_hbox.Add(convert_button,
         #                  flag=wx.ALIGN_RIGHT, proportion=1)
+        # buttons_hbox.AddStretchSpacer()
 
         # buttons_vbox.Add(buttons_hbox,
         #                  flag=wx.EXPAND | wx.RIGHT | wx.LEFT, border=80)
@@ -145,6 +122,6 @@ class MainWindow(wx.Frame):
 
 if __name__ == '__main__':
     app = wx.App()
-    main_win = MainWindow(None)
+    main_win = MainWindow()
 
     app.MainLoop()
