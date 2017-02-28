f61a45e848e70309a5eab6cc2dc09b040db0c5b2
Otto Modinos <ottomodinos@gmail.com>
2017-02-28 13:46:07 +0200

    stuffs

--- app.py
+++ app.py
@@ -12,4 +12,19 @@ WHERE Sell.pharmacy_id = $1;
 STATEMENT2 = """
 SELECT Doctor.*, Patient.name FROM
   Doctor
-  LEFT JOIN Patient ON Doctor.id=Patient.id;"""
+  LEFT JOIN Patient ON Doctor.id = Patient.doctor_id;
+"""
+
+STATEMENT3 = """
+SELECT Patient.name AS patient,
+       Doctor.name AS doctor,
+       Drug.name AS drug,
+       date,
+       dosage
+FROM
+    Prescription
+    JOIN Patient ON Patient.id = Prescription.patient_id
+    JOIN Doctor ON Doctor.id = Prescription.doctor_id
+    JOIN Drug ON Drug.id = Prescription.drug_id
+    JOIN Sell ON Sell.drug_id = Prescription.drug_id AND Sell.pharmacy_id = $1;
+"""
