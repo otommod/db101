788315f4e6790f5b9a1e561017ea08c930469d0f
Otto Modinos <ottomodinos@gmail.com>
2017-03-02 17:10:34 +0200

    move trigger to triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -12,3 +12,19 @@ DROP TRIGGER IF EXISTS before_remove_bigpharma ON BigPharma;
 CREATE TRIGGER before_remove_bigpharma
     BEFORE DELETE ON BigPharma
     FOR EACH ROW EXECUTE PROCEDURE before_remove_bigpharma();
+
+
+CREATE OR REPLACE FUNCTION before_remove_contract() RETURNS trigger AS $$
+    BEGIN
+        DELETE FROM Sell
+            WHERE pharmacy_id = OLD.pharmacy_id AND drug_id = ANY(
+                SELECT id FROM Drug WHERE bigpharma_id = OLD.bigpharma_id
+            );
+        RETURN OLD;
+    END;
+$$ LANGUAGE 'plpgsql';
+
+DROP TRIGGER IF EXISTS before_remove_contract ON Contract;
+CREATE TRIGGER before_remove_contract
+    BEFORE DELETE ON Contract
+    FOR EACH ROW EXECUTE PROCEDURE before_remove_contract();
